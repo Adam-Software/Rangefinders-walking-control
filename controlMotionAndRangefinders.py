749a034a0df6de9042bdddf5d496d8e5f600d236
Alexey Nesterov <vertigra.developer@gmail.com>
2022-08-17 10:27:56 +1000

    Change: divided on private and public function. Added: setMotion function. Added: enable/disable returned array from controller

--- controlMotionAndRangefinders.py
+++ controlMotionAndRangefinders.py
@@ -8,6 +8,7 @@ class AdamMotionAndRangefinders():
 
     def __init__(self):
         super().__init__()
+
         self.ser = SerialU('/dev/ttyAMA0', 115200)
         self.sensors=[0, 0, 0, 0]
 
@@ -18,19 +19,38 @@ class AdamMotionAndRangefinders():
         if isTest:
             return [sensorId, 10, 20, 30]
         else:
-            return self.motion(sensorId, 0, 0)
+            return self._motion(sensorId, 0, 0)
+
+    
+    # if sensorId == 17 or 103
+    # return [0, 0, 0, 0]
+    # [sensorId, distance1, distance2, distance3]
+    # if sensorId != 17 or 103 no data return
+    # if isTest=True and sensorId == 17 or 103 returned example test arreay [sensorId, 10, 20, 30]
+    def setMotion(self, sensorId,  speed1, speed2, isTest=False):
+        
+        if sensorId != 17 and sensorId != 103
+            self._motion(0, speed1, speed2)
+        else
+            data = self._motion(sensorId, speed1, speed2, 255)
+            
+            if isTest:
+                data = [sensorId, 10, 20, 30]
+                return data
+            else
+                return data
 
     # return [0, 0, 0, 0]
     # [sensorId, distance1, distance2, distance3]
     # sensorId = 17/103, left/right
     # speed1 - ??
     # speed2 - ??
-    def motion(self, sensorId, speed1, speed2):
-        speed1 = self.speedAbs(speed1)
-        speed2 = self.speedAbs(speed2)
+    def _motion(self, sensorId, speed1, speed2, enableRangefinders=0):
+        speed1 = self._speedAbs(speed1)
+        speed2 = self._speedAbs(speed2)
 
-        data = [18, sensorId, self.speedShift(speed1), self.speedMasking(speed1), self.speedShift(speed2), self.speedMasking(speed2), 255]
-        data.append(self.CRC8(data[1:]))
+        data = [18, sensorId, self._speedShift(speed1), self._speedMasking(speed1), self._speedShift(speed2), self._speedMasking(speed2), enableRangefinders]
+        data.append(self._CRC8(data[1:]))
         data.append(36)
         self.ser.write(data, len(data))
         time.sleep(0.02)
@@ -57,24 +77,23 @@ class AdamMotionAndRangefinders():
          
         return self.sensors
     
-    #Utils
-
-    def speedAbs(self, speed):
+    #Servo utils 
+    def _speedAbs(self, speed):
         if speed >= 0:
             return speed
         if speed < 0:
             return 65534 - abs(speed)
         
-    
-    def speedShift(self, speed):
+    #Byte utils
+    def _speedShift(self, speed):
         return speed >> 8 
 
-    def speedMasking(self, speed):
+    #Byte utils
+    def _speedMasking(self, speed):
         return speed & 0xff
 
     #read/write serial
-
-    def CRC8(self, mas):
+    def _CRC8(self, mas):
         st_byt = 0
         crc = 0
         while st_byt < len(mas):
