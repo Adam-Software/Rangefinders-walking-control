ce877df834fabdcf89bba8d638572d9f1c3621c6
Alexey Nesterov <vertigra.developer@gmail.com>
2022-08-30 08:54:12 +1000

    Update exampleMotionAndRangefinders.py

--- exampleMotionAndRangefinders.py
+++ exampleMotionAndRangefinders.py
@@ -4,6 +4,8 @@ from motionAndRangefinders import MotionAndRangefinders
 
 pp = MotionAndRangefinders()
 
+# distance
+
 #имитация возврата резульатов, имитирующих реальные данные [103, 10, 20, 30]
 left_sensor = pp.getDistance(17, True)
 time.sleep(0.1)
@@ -28,40 +30,44 @@ time.sleep(0.1)
 print('Get distance with real return value')
 print(right_sensor)
 
-#едем вперед и не возврящаем значение 
+# motion
+
+#едем вперед и не возвращаем значение 
 print('Call motion with speed1 = 2000 and speed2 = 2000 no return value')
 time.sleep(0.1)
-motion = pp.setMotion(0, 2000, 2000)
+motion = pp.setMotion(17, 2000, 2000)
+motion = pp.setMotion(103, 2000, 2000)
 print(motion)
 
 #едем назад и не возврящаем значение 
 print('Call motion with speed1 = -2000 and speed2 = -2000 no return value')
 time.sleep(0.1)
-motion = pp.setMotion(0, -2000, -2000)
+motion = pp.setMotion(17, -2000, -2000)
+motion = pp.setMotion(103, -2000, -2000)
 print(motion)
 
 #едем вперед и возврящаем реальные значение с сенсора 17 
 print('Call motion with sensor 17 speed1 = 2000 and speed2 = 2000 and return rangefinders data ([0, 0, 0, 0] if uart disconect)') 
 time.sleep(0.1)
-motion = pp.setMotion(17, 2000, 2000)
+motion = pp.setMotion(17, 2000, 2000, True)
 print(motion)
 
 #едем вперед и возврящаем реальные значение с сенсора 103
 print('Call motion with sensor 103 speed1 = 2000 and speed2 = 2000 and return rangefinders data ([0, 0, 0, 0] if uart disconect)') 
 time.sleep(0.1)
-motion = pp.setMotion(103, 2000, 2000)
+motion = pp.setMotion(103, 2000, 2000, True)
 print(motion)
 
 #едем вперед и возврящаем тестовые значения 
 print('Call motion with sensor 17  speed1 = 2000 and speed2 = 2000 and return rangefinders test array [17, 10, 20, 30]') 
 time.sleep(0.1)
-motion = pp.setMotion(17, 2000, 2000, True)
+motion = pp.setMotion(17, 2000, 2000, True, True)
 print(motion)
 
 #едем вперед и возврящаем тестовые значения 
 print('Call motion with sensor 103 speed1 = 2000 and speed2 = 2000 and return rangefinders test array [103, 10, 20, 30]') 
 time.sleep(0.1)
-motion = pp.setMotion(103, 2000, 2000, True)
+motion = pp.setMotion(103, 2000, 2000, True, True)
 print(motion)
 
 #едем вперед и указываем неверный сенсор
